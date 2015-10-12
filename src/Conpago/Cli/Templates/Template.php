cfbff8e403c7780d24064523b298f825931d4d5b
Bartosz Gołek <bartosz.golek@gmail.com>
2015-10-13 00:29:34 +0200

    templates and test implementation of algorithm

--- src/Conpago/Cli/Templates/Template.php
+++ src/Conpago/Cli/Templates/Template.php
@@ -10,6 +10,10 @@
 
 
 	class Template {
+		const START_CHAR = "{";
+		const END_CHAR = "}";
+		const START_SEQ = "{{";
+		const END_SEQ = "}}";
 		/**
 		 * @var ITemplateLoader
 		 */
@@ -27,70 +31,165 @@
 
 		function render($template, $variables = [])
 		{
-			$content = $this->loader->load( $template );
+			$content = $this->loader->load($template);
 			$content = $this->fill($content, $variables);
 			return $this->normalize_endings($content, $this->options->getLineEndings());
 		}
 
-		private function normalize_endings( $content, $line_endings ) {
+		private function normalize_endings($content, $line_endings) {
 			$content = str_replace("\r\n", "\n", $content);
 			if ($line_endings != "\n")
 				$content = str_replace("\n", $line_endings, $content);
 			return $content;
 		}
 
-		private function fill( $content, $variables ) {
-			$in  = fopen( 'php://memory', 'w' );
-			$out = fopen( 'php://memory', 'w' );
-			fwrite( $in, $content );
-			fseek( $in, 0 );
+		private function fill($content, $variables) {
+			return (new TemplateFiller($content, $variables))->fill();
+		}
+	}
 
-			$index = 0;
-			$variable = "";
-			$variable_start = - 1;
-			$possible_variable_start = - 1;
-			$variable_end = -1;
-			$possible_variable_end = -1;
-			while ( feof( $in ) === false ) {
-				$char = fread( $in, 1 );
-				if ($variable_start != -1 && $char == '}' ) {
-					$possible_variable_end = $index;
-				}
-				else if ($possible_variable_end != -1 && $char == '}' ) {
-					$variable_end = $possible_variable_end;
-				}
-				else if ( $possible_variable_start != - 1 && $char == '{' ) {
-					$variable_start = $possible_variable_start;
-				}
-				else if ( $possible_variable_start != - 1 && $char != '{' ) {
-					$possible_variable_start = -1;
-					fwrite( $out, $char );
-				}
-				else if ( $char == '{' ) {
-					$possible_variable_start = $index;
-				}
+	class TemplateFiller {
+		protected $in;
+		protected $out;
+		protected $variable = "";
+		protected $in_variable = false;
+		protected $possible_variable_start = false;
+		protected $at_variable_end = false;
+		protected $possible_variable_end = false;
+		protected $variables;
+		protected $call_stack;
 
-				if ($variable_start != -1 && $variable_end == -1) {
-					$variable .= $char;
-				}
-				if ($variable_start != -1 && $variable_end != -1) {
-					$variable = substr( $variable, 2, -2);
-					fwrite($out, $variables[$variable]);
-					$variable = "";
-					$variable_start = - 1;
-					$possible_variable_start = - 1;
-					$variable_end = -1;
-					$possible_variable_end = -1;
-				}
-				else
-					fwrite( $out, $char );
+		/**
+		 * @param $content
+		 * @param $variables
+		 * @param $call_stack
+		 */
+		function __construct( $content, $variables, $call_stack = []) {
+			$this->in  = $this->prepareInStream( $content );
+			$this->out = $this->prepareOutStream();
 
-				$index++;
+			$this->call_stack = $call_stack;
+			$this->variables  = $variables;
+		}
+
+		function fill() {
+			while ( feof( $this->in ) === false) {
+				$char = fread( $this->in, 1);
+
+				if ($this->possible_variable_end == true && !$this->isEndChar($char)) {
+					$this->possible_variable_end = false;
+				}
+				else if ($this->possible_variable_end == false && $this->in_variable == true && $this->isEndChar($char)) {
+					$this->possible_variable_end = true;
+				}
+				else if ( $this->possible_variable_end == true && $this->at_variable_end == false && $this->isEndChar($char)) {
+					$this->at_variable_end = true;
+					$this->variable .= $char;
+				}
+				else if ( $this->possible_variable_start == true && $this->isStartChar($char)) {
+					$this->in_variable = $this->possible_variable_start;
+				}
+				else if ( $this->possible_variable_start == true && $this->in_variable == false && !$this->isStartChar($char)) {
+					$this->possible_variable_start = false;
+					$this->addToOutput($this->variable);
+					$this->variable = "";
+				}
+				else if ($this->isStartChar($char)) {
+					$this->possible_variable_start = true;
+					$this->variable .= $char;
+				}
+				$this->collectVariable( $char );
+				if ($this->in_variable == true && $this->at_variable_end == true) {
+					$this->addToOutput($this->getVariableReplacement());
+					$this->resetState();
+				}
+				else if ($this->possible_variable_start == false) {
+					$this->addToOutput($char);
+				}
 			}
-			//foreach ($variables as $variable => $value)
-			//	$result = str_replace("{{".$variable."}}", $value, $result);
+			$this->handleBrokenVariable();
+			return $this->getResult();
+		}
 
-			fseek($out, 0);
-			return fread($out, 8192);
+		private function addToOutput($value)
+		{
+			fwrite( $this->out, $value);
+		}
+
+		private function prepareInStream($content) {
+			$in = fopen('php://memory', 'w');
+			fwrite($in, $content);
+			fseek($in, 0);
+
+			return $in;
+		}
+
+		private function prepareOutStream() {
+			return fopen('php://memory', 'w');
+		}
+
+		/**
+		 * @param $char
+		 *
+		 * @return bool
+		 */
+		private function isStartChar($char) {
+			return $char == Template::START_CHAR;
+		}
+
+		/**
+		 * @param $char
+		 *
+		 * @return bool
+		 */
+		private function isEndChar($char) {
+			return $char == Template::END_CHAR;
+		}
+
+		private function getResult() {
+			fseek($this->out, 0);
+			return fread($this->out, 8192);
+		}
+
+		private function resetState() {
+			$this->variable                = "";
+			$this->in_variable             = false;
+			$this->possible_variable_start = false;
+			$this->at_variable_end         = false;
+			$this->possible_variable_end   = false;
+			$this->call_stack              = [ ];
+		}
+
+		private function getVariableReplacement() {
+			$cut_variable = substr( $this->variable, 2, -2);
+			$replacement = $cut_variable;
+			if ( array_key_exists( $cut_variable, $this->variables ) ) {
+				$recursion          = in_array( $cut_variable, $this->call_stack );
+				$this->call_stack[] = $cut_variable;
+				if ( $recursion ) {
+					throw new RecursionTemplateException( $this->call_stack );
+				}
+
+				$replacement = (new TemplateFiller($this->variables[ $cut_variable ], $this->variables, $this->call_stack))->fill();
+
+				return $replacement;
+			}
+
+			return $replacement;
+		}
+
+		/**
+		 * @param $char
+		 */
+		private function collectVariable( $char ) {
+			if ( $this->in_variable == true && $this->at_variable_end == false ) {
+				$this->variable .= $char;
+			}
+		}
+
+		private function handleBrokenVariable() {
+			if ( $this->in_variable ) {
+				$this->addToOutput( $this->variable );
+			}
 		}
 	}
--- tests/Conpago/Cli/Templates/TemplateTest.php
+++ tests/Conpago/Cli/Templates/TemplateTest.php
@@ -25,30 +25,30 @@
 		 */
 		protected $options;
 
-//		function testRenderWillReturnContentFromLoader()
-//		{
-//			$this->loader->expects( $this->any() )->method( 'load' )->willReturn( 'temp' );
-//			$rendered = $this->template->render('dummy');
-//			$this->assertEquals('temp', $rendered);
-//		}
-//
-//		function testRenderWillNormalizeLineEndingsToWindows()
-//		{
-//			$this->loader->expects($this->any())->method('load')->willReturn("temp\ntemp\r\ntemp");
-//			$this->options->expects($this->any())->method('getLineEndings')->willReturn("\r\n");
-//
-//			$rendered = $this->template->render('dummy');
-//			$this->assertEquals("temp\r\ntemp\r\ntemp", $rendered);
-//		}
-//
-//		function testRenderWillNormalizeLineEndingsToLinux()
-//		{
-//			$this->loader->expects($this->any())->method('load')->willReturn("temp\ntemp\r\ntemp");
-//			$this->options->expects($this->any())->method('getLineEndings')->willReturn("\n");
-//
-//			$rendered = $this->template->render('dummy');
-//			$this->assertEquals("temp\ntemp\ntemp", $rendered);
-//		}
+		function testRenderWillReturnContentFromLoader()
+		{
+			$this->loader->expects( $this->any() )->method( 'load' )->willReturn( 'temp' );
+			$rendered = $this->template->render('dummy');
+			$this->assertEquals('temp', $rendered);
+		}
+
+		function testRenderWillNormalizeLineEndingsToWindows()
+		{
+			$this->loader->expects($this->any())->method('load')->willReturn("temp\ntemp\r\ntemp");
+			$this->options->expects($this->any())->method('getLineEndings')->willReturn("\r\n");
+
+			$rendered = $this->template->render('dummy');
+			$this->assertEquals("temp\r\ntemp\r\ntemp", $rendered);
+		}
+
+		function testRenderWillNormalizeLineEndingsToLinux()
+		{
+			$this->loader->expects($this->any())->method('load')->willReturn("temp\ntemp\r\ntemp");
+			$this->options->expects($this->any())->method('getLineEndings')->willReturn("\n");
+
+			$rendered = $this->template->render('dummy');
+			$this->assertEquals("temp\ntemp\ntemp", $rendered);
+		}
 
 		function testRenderWillReplaceVariableWithValue()
 		{
@@ -59,32 +59,91 @@
 			$this->assertEquals("value", $rendered);
 		}
 
-//		function testRenderWillReplaceMutipleVariablesWithValue()
-//		{
-//			$this->loader->expects($this->any())->method('load')->willReturn("{{variable}}{{variable2}}");
-//			$this->options->expects($this->any())->method('getLineEndings')->willReturn("\n");
-//
-//			$rendered = $this->template->render('dummy', ['variable' => 'value', 'variable2' => 'value2']);
-//			$this->assertEquals("valuevalue2", $rendered);
-//		}
-//
-//		function testRenderWillReplaceVariableInVariableWithValue()
-//		{
-//			$this->loader->expects($this->any())->method('load')->willReturn("{{variable}}{{variable2}}");
-//			$this->options->expects($this->any())->method('getLineEndings')->willReturn("\n");
-//
-//			$rendered = $this->template->render('dummy', ['variable' => '{{variable2}}', 'variable2' => 'value2']);
-//			$this->assertEquals("value2value2", $rendered);
-//		}
-//
-//		function testRenderWillReplaceVariableInLaterDefinedVariableWithValue()
-//		{
-//			$this->loader->expects($this->any())->method('load')->willReturn("{{variable}}{{variable2}}");
-//			$this->options->expects($this->any())->method('getLineEndings')->willReturn("\n");
-//
-//			$rendered = $this->template->render('dummy', ['variable' => 'value', 'variable2' => '{{variable}}']);
-//			$this->assertEquals("value2value2", $rendered);
-//		}
+		function testRenderWillReplaceVariableInTextWithValue()
+		{
+			$this->loader->expects($this->any())->method('load')->willReturn("xxx{{variable}}zzz");
+			$this->options->expects($this->any())->method('getLineEndings')->willReturn("\n");
+
+			$rendered = $this->template->render('dummy', ['variable' => 'value']);
+			$this->assertEquals("xxxvaluezzz", $rendered);
+		}
+
+		function testRenderWillReplaceMutipleVariablesWithValue()
+		{
+			$this->loader->expects($this->any())->method('load')->willReturn("{{variable}}{{variable2}}");
+			$this->options->expects($this->any())->method('getLineEndings')->willReturn("\n");
+
+			$rendered = $this->template->render('dummy', ['variable' => 'value', 'variable2' => 'value2']);
+			$this->assertEquals("valuevalue2", $rendered);
+		}
+
+		function testRenderWillReplaceVariableInVariableWithValue()
+		{
+			$this->loader->expects($this->any())->method('load')->willReturn("{{variable}}{{variable2}}");
+			$this->options->expects($this->any())->method('getLineEndings')->willReturn("\n");
+
+			$rendered = $this->template->render('dummy', ['variable' => '{{variable2}}', 'variable2' => 'value2']);
+			$this->assertEquals("value2value2", $rendered);
+		}
+
+		function testRenderWillReplaceVariableInLaterDefinedVariableWithValue()
+		{
+			$this->loader->expects($this->any())->method('load')->willReturn("{{variable}}{{variable2}}");
+			$this->options->expects($this->any())->method('getLineEndings')->willReturn("\n");
+
+			$rendered = $this->template->render('dummy', ['variable' => 'value', 'variable2' => '{{variable}}']);
+			$this->assertEquals("valuevalue", $rendered);
+		}
+
+		function testRenderWillNotReplaceBraceIfIsNotVariableDefinition()
+		{
+			$this->loader->expects($this->any())->method('load')->willReturn("{vari{{variable2}}");
+			$this->options->expects($this->any())->method('getLineEndings')->willReturn("\n");
+
+			$rendered = $this->template->render('dummy', ['variable' => 'value', 'variable2' => 'value2']);
+			$this->assertEquals("{varivalue2", $rendered);
+		}
+
+		function testRenderWillNotReplaceIfEndBracesAreSeparate()
+		{
+			$this->loader->expects($this->any())->method('load')->willReturn("{{variable2}x}");
+			$this->options->expects($this->any())->method('getLineEndings')->willReturn("\n");
+
+			$rendered = $this->template->render('dummy', ['variable' => 'value', 'variable2' => 'value2']);
+			$this->assertEquals("{{variable2}x}", $rendered);
+		}
+
+		/**
+		 * @expectedException Conpago\Cli\Templates\RecursionTemplateException
+		 * @expectedExceptionMessage Template contains recursiv variables: variable > variable2 > variable
+		 */
+		function testRenderWillReplaceVariableRecursiveInVariablesWillThrowExceptionWithValue()
+		{
+			$this->loader->expects($this->any())->method('load')->willReturn("{{variable}}{{variable2}}");
+			$this->options->expects($this->any())->method('getLineEndings')->willReturn("\n");
+
+			$this->template->render('dummy', ['variable' => '{{variable2}}', 'variable2' => '{{variable}}']);
+		}
+
+		/**
+		 * @expectedException Conpago\Cli\Templates\RecursionTemplateException
+		 * @expectedExceptionMessage Template contains recursiv variables: variable > variable2 > variable3 > variable4 > variable
+		 */
+		function testRenderWillReplaceVariableDeepRecursiveInVariablesWillThrowExceptionWithValue()
+		{
+			$this->loader->expects($this->any())->method('load')->willReturn("{{variable}}{{variable2}}");
+			$this->options->expects($this->any())->method('getLineEndings')->willReturn("\n");
+
+			$this->template->render(
+					'dummy',
+					[
+							'variable' => '{{variable2}}',
+							'variable2' => '{{variable3}}',
+							'variable3' => '{{variable4}}',
+							'variable4' => '{{variable}}'
+					]
+			);
+		}
 
 		protected function setUp() {
 			$this->loader = $this->getMock( 'Conpago\Cli\Templates\ITemplateLoader' );
