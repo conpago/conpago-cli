b5847a2fa07d03713243c2b9f14763754923cd77
Bartosz Gołek <bartosz.golek@gmail.com>
2015-11-30 23:40:42 +0100

    some FeatureContext implementation

---
+++ features/bootstrap/TestApplicationFactory.php
@@ -0,0 +1,21 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: bg
+	 * Date: 28.11.15
+	 * Time: 12:36
+	 */
+
+	/**
+	 *
+	 * @license MIT
+	 * @author Bartosz Gołek <bartosz.golek@gmail.com>
+	 **/
+	class TestApplicationFactory {
+
+		/**
+		 * TestApplicationFactory constructor.
+		 */
+		public function __construct() {
+		}
+	}
